152c6e3f4ad63f1f63057f0ba0ed2f8f157b50c4
Vo Thu Dieu <dieu110537@donga.edu.vn>
2024-10-26 23:55:18 +0700

    Add files via upload

--- THỰC HÀNH/TUẦN 3/dieu110537.java
+++ THỰC HÀNH/TUẦN 3/dieu110537.java
@@ -1,34 +1,25 @@
-package vothudieu;
-
-import java.util.Scanner;
+package buoi3;
 
 public class dieu110537 {
-
     public static String gioiTinh(boolean gt) {
-    return gt ? "Nam" : "Nữ";
-}
-
-public static void main(String[] args) {
-    String maSinhVien = "110537", hoVaTen = "Võ Thu Diệu", ngaySinh = "12/10/2006", lop = "GD24A",
-     khoa = "Mỹ Thuật Ứng Dụng", diachi = "Đà Nẵng";
-     int tuoi = 18;
-     boolean gt = false;
-     System.out.println("-----THÔNG TIN SINH VIÊN-----");
-     System.out.println("Mã sinh viên: " + maSinhVien);
-     System.out.println("Họ và tên: " + hoVaTen);
-     System.out.println("Ngày sinh: " + ngaySinh);
-     System.out.println("Tuổi: " + tuoi);
-     System.out.println("Giới tính: " + gioiTinh(false));
-     System.out.println("Lớp: " + lop);
-     System.out.println("Khoa: " + khoa);
-     System.out.println("Địa Chỉ: " + diachi);
-     System.err.println("------HẾT------");
-     String hTen;
-     try (Scanner sc = new Scanner(System.in)) {
-        System.out.println("Nhập họ và tên ");
-         hTen = sc.nextLine();
+        return gt ? "Nam" : "Nữ";
     }
-     System.out.println("Họ và tên " + hTen);
-     
-}
+
+
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        String maSinhVien = "110537", hoVaTen = "Võ Thu Diệu", ngaySinh = "12/10/2006", lop = "GD24A",
+        Khoa ="Mỹ Thuật Ứng Dụng", diaChi = "Đà Nẵng";
+        int tuoi = 18;
+        boolean gt = false;
+        System.out.println("-----THÔNG TIN SINH VIÊN-----");
+        System.out.println("Mã sinh viên : " + maSinhVien);
+        System.out.println("Họ và Tên :" + hoVaTen);
+        System.out.println("tuổi : " + tuoi);
+        System.out.println("Giới tính : " + gioiTinh(gt));
+        System.out.println("Lớp " + lop);
+        System.out.println("Khoa " + Khoa);
+        System.out.println("Địa chỉ: " + diaChi);
+        System.out.println("-----HẾT-----");
+    }   
 }
