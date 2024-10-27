bbc82b9c7f877ea46af11286fe5fcec625dd4941
Vo Thu Dieu <dieu110537@donga.edu.vn>
2024-10-27 14:36:55 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 4/Buoi4.java
@@ -0,0 +1,22 @@
+package dieu110537;
+
+public class Buoi4 {
+        public static void bai4_1() {
+        int arr[] = {3, 5, 8, 10, 14, 25};
+        int cs=0;
+        System.out.printf("Phần tử thứ nhất arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ hai arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ ba arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ tư arr[%d] = " + arr[cs], cs++);
+        System.out.printf("\nPhần tử thứ năm arr[%d] =  " + arr[cs], cs++);
+        System.out.printf("n phần tử thứ năm arr[%d] = " + arr[cs], cs++);
+
+ 
+    }
+
+public static void main(String[] args) {
+         // TODO Auto-generated method stub
+         bai4_1();
+}
+
+}
