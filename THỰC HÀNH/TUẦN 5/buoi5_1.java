e5230968cdb3c95ab2245cffae504143744b3741
Vo Thu Dieu <dieu110537@donga.edu.vn>
2024-10-28 22:38:29 +0700

    Add files via upload

---
+++ THỰC HÀNH/TUẦN 5/buoi5_1.java
@@ -0,0 +1,13 @@
+package dieu110537;
+
+public class buoi5_1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int max = (a > b) ? a : b;
+        System.out.println("Giá trị lớn nhất: " + max);
+        int min = (a < b) ? a : b;
+        System.out.println("Giá trị nhỏ nhất: " + min);
+    }
+}
